7753bde25fc80802c56a269dc9d1b9153615f3ad
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2025-05-30 17:56:18 -0400

    updated nue plots after the refactor

--- studies/nue_cc_inclusive/plot_vars.py
+++ studies/nue_cc_inclusive/plot_vars.py
@@ -1,32 +1,22 @@
 import os,sys
 import ROOT as rt
 
-"""
-2025-05-25 04:35:20,426 - LanternAna - INFO - Loaded dataset 'run1_bnb_nue_overlay_mcc9_v28_wctagger' with 14804 entries
-2025-05-25 04:35:20,427 - LanternAna - INFO -   MC dataset with 1.7586890825003933e+22 POT
-Adding to dataset[EventTree] to Tree[EventTree]: /home/twongjirad/working/larbys/gen2/container_u20_env/work/lantern_ana/ntuple_mcc9_v28_wctagger_bnboverlay_v3dev_reco_retune.root
-2025-05-25 04:35:20,454 - LanternAna - INFO - Loaded dataset 'run1_bnb_nu_overlay_mcc9_v28_wctagger' with 69741 entries
-2025-05-25 04:35:20,454 - LanternAna - INFO -   MC dataset with 1.7458035848006284e+20 POT
-Adding to dataset[EventTree] to Tree[EventTree]: /home/twongjirad/working/larbys/gen2/container_u20_env/work/lantern_ana/ntuple_mcc9_v29e_dl_run1_C1_extbnb_v3dev_reco_retune.root
-2025-05-25 04:35:20,471 - LanternAna - INFO - Loaded dataset 'run1_extbnb_mcc9_v29e_C1' with 42147 entries
-Adding to dataset[EventTree] to Tree[EventTree]: /home/twongjirad/working/larbys/gen2/container_u20_env/work/lantern_ana/ntuple_mcc9_v28_wctagger_bnb5e19_v3dev_reco_retune.root
-2025-05-25 04:35:20,542 - LanternAna - INFO - Loaded dataset 'run1_bnb5e19' with 176222 entries
-2025-05-25 04:35:20,542 - LanternAna - INFO - Processing dataset: run1_extbnb_mcc9_v29e_C1
-2025-05-25 04:35:20,572 - LanternAna - INFO - Processing 42147 events...
-"""
 
 """
 """
 targetpot = 4.4e19
-samples = ['nue','numu','extbnb','data']
+samples = ['nue_sig','nue_bg','numu','extbnb','data']
+#samples = ['nue','numu']
 scaling = {"numu":targetpot/4.5221966264744385e+20,
-           "nue":targetpot/1.0696499342682672e+22,
-           "extbnb":0.47809891*0.80,
+           "nue_sig":targetpot/1.0696499342682672e+22,
+           "nue_bg":targetpot/1.0696499342682672e+22,
+           "extbnb":(176222.0/368589)*0.8,
            "data":1.0}
-files = {"numu":"./output/run1_bnb_nu_overlay_mcc9_v28_wctagger_20250527_161522.root",
-         "nue":"./output/run1_bnb_nue_overlay_mcc9_v28_wctagger_20250526_203902.root",
-         "extbnb":"./output/run1_extbnb_mcc9_v29e_C1_20250526_203934.root",
-         "data":"./output/run1_bnb5e19_20250526_204132.root"}
+files = {"numu":"./output/run1_bnb_nu_overlay_mcc9_v28_wctagger_20250530_150819.root",
+         "nue_sig":"./output/run1_bnb_nue_overlay_mcc9_v28_wctagger_20250530_150752.root",
+         "nue_bg":"./output/run1_bnb_nue_overlay_mcc9_v28_wctagger_20250530_150752.root",
+         "extbnb":"./output/run1_extbnb_mcc9_v29e_C1_20250530_151509.root",
+         "data":"./output/run1_bnb5e19_20250530_151920.root"}
 tfiles = {}
 trees = {}
 
@@ -40,46 +30,37 @@ for sample in samples:
 
 out = rt.TFile("temp.root","recreate")
 
-vars = [('nuselvar_emax_econfidence', 40, 0, 20, 'electron confidence', 1),
-        ('nuselvar_emax_primary_score',40,0,1.0,'primary score', 1),
-        ('nuselvar_emax_fromneutral_score',40,0,1.0,'from neutral parent score', 1),
-        ('nuselvar_emax_fromcharged_score',40,0,1.0,'from charged parent score', 1),
-        ('nuselvar_emax_el_normedscore',40,0,1.0,'electron score (normalized)', 1),
-        ('visible_energy',15,0,3000,'visible energy; MeV', 0),
-        ('nuselvar_nMuTracks',20,0,20,'number of mu-like track-prongs',1),
-        ('nuselvar_max_muscore',42,-20,1,'max mu-like score',1),
-        ('nuselvar_vtx_kpscore',60,0.4,1.0,'keypoint score',1),
-        ('nuselvar_vtx_cosmicfrac',50,0,1.01,'fraction of pixels near vertex',1),
+vars = [
+    ('vertex_properties_score',60,0.4,1.0,'keypoint score',0),
+    ('vertex_properties_cosmicfrac',50,0,1.01,'fraction of pixels near vertex',0),
+    ('vertex_properties_frac_outoftime_pixels',50,0,1.01,'fraction of reco pixels labed out of time',1),
+    ('vertex_properties_frac_intime_unreco_pixels',50,0,1.01,'fraction of unreco\'d pixels in event are in-time',1),
+    ('recoElectron_emax_econfidence', 40, 0, 20, 'electron confidence', 1),
+    ('recoElectron_emax_primary_score',40,0,1.0,'primary score', 1),
+    ('recoElectron_emax_fromneutral_score',40,0,1.0,'from neutral parent score', 1),
+    ('recoElectron_emax_fromcharged_score',40,0,1.0,'from charged parent score', 1),
+    ('recoElectron_emax_el_normedscore',40,0,1.0,'electron score (normalized)', 1),
+    ('recoElectron_emax_charge',50,0,1000e3,'electron charge',1),
+    ('visible_energy',15,0,3000,'visible energy; MeV', 0),
+    ('recoMuonTrack_nMuTracks',20,0,20,'number of mu-like track-prongs',0),
+    ('recoMuonTrack_max_muscore',42,-20,1,'max mu-like score',1),
+    ('recoMuonTrack_max_mucharge',100,0,100e3,'max mu-like charge',1),
 ]
+
 hists = {}
 canvs = {}
 
 
-cut = "(reco_nue_CCinc_cutresult==1)"
-cut += " && (nuselvar_max_muscore<-2.0)"
-cut += " && (nuselvar_vtx_cosmicfrac<0.20)"
-cut += " && (nuselvar_emax_el_normedscore>0.95)"
-cut += " && (nuselvar_emax_fromcharged_score<0.05)"
-cut += " && (nuselvar_emax_primary_score>0.8)"
-cut += " && (nuselvar_vtx_kpscore>0.80)"
-cut += " && (nuselvar_emax_econfidence>5.0)"
-cut += " && (visible_energy>0.05)"
-# cut += " && (nuselvar_emax_fromneutral_score<0.10)"
-
-#cut = "(nuselvar_vtx_kpscore>0.0)"
-
-
-
-
-#cut += " && (nuselvar_nMuTracks<1) "
-#
-
-#cut += " && (nuselvar_max_muscore<-1.0)"
-
-
-
-
-#cut += " && (nuselvar_max_muscore>-1000.0 && nuselvar_max_muscore<-2.0)"
+cut = "(vertex_properties_found==1 && vertex_properties_infiducial)"
+cut += " && (vertex_properties_cosmicfrac<0.15)"
+cut += " && (recoMuonTrack_max_muscore<-2.5)"
+cut += " && (recoElectron_emax_primary_score>0.7)"
+cut += " && (vertex_properties_frac_intime_unreco_pixels<0.9)"
+cut += " && (recoElectron_emax_el_normedscore>0.9)"
+cut += " && (recoElectron_emax_fromcharged_score<0.05)"
+cut += " && (vertex_properties_score>0.85)"
+cut += " && (vertex_properties_frac_outoftime_pixels>0.75)"
+cut += " && (recoElectron_emax_econfidence>6.0)"
 
 for var, nbins, xmin, xmax, htitle, setlogy in vars:
 
@@ -91,31 +72,51 @@ for var, nbins, xmin, xmax, htitle, setlogy in vars:
         hname = f'h{var}_{sample}'
         hists[(var,sample)] = rt.TH1D( hname, "", nbins, xmin, xmax )
         print("fill ",hname)
-        trees[sample].Draw(f"{var}>>{hname}",f"({cut})*eventweight_weight")
+
+        samplecut = cut
+        if sample=="nue_sig":
+            samplecut += " && (nueCCinc_is_target_nuecc_inclusive==1)"
+        elif sample=="nue_bg":
+            samplecut += " && (nueCCinc_is_target_nuecc_inclusive!=1)"
+
+        trees[sample].Draw(f"{var}>>{hname}",f"({samplecut})*eventweight_weight")
         hists[(var,sample)].Scale( scaling[sample] )
         print(f"{var}-{sample}: ",hists[(var,sample)].Integral())
     
-    hists[(var,"nue")].SetFillColor(rt.kRed)
-    hists[(var,"nue")].SetFillStyle(3001)
-    hists[(var,"numu")].SetFillColor(rt.kBlue-3)
-    hists[(var,"numu")].SetFillStyle(3003)
-    hists[(var,"extbnb")].SetFillColor(rt.kGray)
-    hists[(var,"extbnb")].SetFillStyle(3144)
-    hists[(var,"data")].SetLineColor(rt.kBlack)
-    hists[(var,"data")].SetLineWidth(2)
+    hists[(var,"nue_sig")].SetFillColor(rt.kRed)
+    hists[(var,"nue_sig")].SetFillStyle(3001)
+    hists[(var,"nue_bg")].SetFillColor(rt.kRed-4)
+    hists[(var,"nue_bg")].SetFillStyle(3001)
+    if (var,'numu') in hists:
+        hists[(var,"numu")].SetFillColor(rt.kBlue-3)
+        hists[(var,"numu")].SetFillStyle(3003)
+    if (var,'extbnb') in hists:
+        hists[(var,"extbnb")].SetFillColor(rt.kGray)
+        hists[(var,"extbnb")].SetFillStyle(3144)
+    if (var,'data') in hists:
+        hists[(var,"data")].SetLineColor(rt.kBlack)
+        hists[(var,"data")].SetLineWidth(2)
 
     hstack_name = f"hs_{var}"
     hstack = rt.THStack(hstack_name,"")
-    hstack.Add( hists[(var,"extbnb")])
-    hstack.Add( hists[(var,"numu")])
-    hstack.Add( hists[(var,"nue")])
+    if (var,'extbnb') in hists:
+        hstack.Add( hists[(var,"extbnb")])
+    if (var,'numu') in hists:
+        hstack.Add( hists[(var,"numu")])
+    if (var,'nue_bg') in hists:
+        hstack.Add( hists[(var,"nue_bg")])
+    if (var,'nue_sig') in hists:
+        hstack.Add( hists[(var,"nue_sig")])
     hists[(hstack_name,sample)] = hstack
 
     hstack.Draw("hist")
     canvs[var].SetLogy(setlogy)
 
     predmax = hstack.GetMaximum()
-    datamax = hists[(var,"data")].GetMaximum()
+    if (var,"data") in hists:
+        datamax = hists[(var,"data")].GetMaximum()
+    else:
+        datamax = -1.0
 
     if predmax>datamax:
         if setlogy==1:
@@ -129,7 +130,8 @@ for var, nbins, xmin, xmax, htitle, setlogy in vars:
         hists[(var,"data")].Draw("E1")
         hstack.Draw("histsame")
 
-    hists[(var,"data")].Draw("E1same")
+    if (var,'data') in hists:
+        hists[(var,"data")].Draw("E1same")
 
     canvs[var].Update()
 
