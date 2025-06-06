07de6eae627dcd417edfdf3d352800bb39da9a28
twongjirad <taritree.wongjirad@gmail.com>
2025-06-06 15:18:25 -0400

    dont use a clean environment to preserve display variable

--- start_tufts_container.sh
+++ start_tufts_container.sh
@@ -5,4 +5,4 @@ module load apptainer/1.2.4-suid
 
 BINDING_FOLDERS=/cluster/tufts/,/cluster/tufts/
 #singularity shell --cleanenv -B ${BINDING_FOLDERS} /cluster/tufts/wongjiradlabnu/nutufts/containers/lantern_v2_me_06_03_prod/
-apptainer shell --cleanenv -B ${BINDING_FOLDERS} /cluster/tufts/wongjiradlabnu/twongj01/gen2/photon_analysis/u20.04_cu111_cudnn8_torch1.9.0_minkowski.sif
+apptainer shell -B ${BINDING_FOLDERS} /cluster/tufts/wongjiradlabnu/twongj01/gen2/photon_analysis/u20.04_cu111_cudnn8_torch1.9.0_minkowski.sif
