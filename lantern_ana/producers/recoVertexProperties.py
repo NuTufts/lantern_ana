bd043a6c2c7c187c9e94a31d92a59cf3ca923263
Taritree Wongjirad <taritree.wongjirad@gmail.com>
2025-09-04 01:11:03 +0100

    handle presence or not of flashprediction variables

--- lantern_ana/producers/recoVertexProperties.py
+++ lantern_ana/producers/recoVertexProperties.py
@@ -100,12 +100,14 @@ class VertexPropertiesProducer(ProducerBaseClass):
             self.vertex_vars['frac_outoftime_pixels'][0] = max_outoftime
             self.vertex_vars['frac_intime_unreco_pixels'][0] = max_intime_unreco
 
-            self.vertex_vars['fracerrPE'][0] = ntuple.fracerrPE
-            self.vertex_vars['sinkhorn_div'][0] = ntuple.sinkhorn_div
-            self.vertex_vars['predictedPEtotal'][0] = ntuple.predictedPEtotal
-            self.vertex_vars['observedPEtotal'][0] = ntuple.observedPEtotal
-            self.vertex_vars['vtxKPscore'][0] = ntuple.vtxKPscore
-            self.vertex_vars['vtxKPtype'][0] = ntuple.vtxKPtype
+            if "fracerrPE" in ntuple.__dict__:
+                # Has new flashmatch variables in the tree
+                self.vertex_vars['fracerrPE'][0] = ntuple.fracerrPE
+                self.vertex_vars['sinkhorn_div'][0] = ntuple.sinkhorn_div
+                self.vertex_vars['predictedPEtotal'][0] = ntuple.predictedPEtotal
+                self.vertex_vars['observedPEtotal'][0] = ntuple.observedPEtotal
+                self.vertex_vars['vtxKPscore'][0] = ntuple.vtxKPscore
+                self.vertex_vars['vtxKPtype'][0] = ntuple.vtxKPtype
             
             # MC truth distance if available
             if ismc:
