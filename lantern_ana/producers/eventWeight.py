a3657edc9e475ddf7f92daa83176d5271601c408
Taritree Wongjirad <taritree@mit.edu>
2026-01-07 11:20:43 -0600

    modified eventweight producer to allow for options for reading genie tune event weight from pandora tree

--- lantern_ana/producers/eventWeight.py
+++ lantern_ana/producers/eventWeight.py
@@ -17,15 +17,20 @@ class eventWeightProducer(ProducerBaseClass):
     def __init__(self, name, config):
         super().__init__(name, config)
         self.eventweight = array('f', [1.0])
+        self.goodweight  = array('i', [1])
+        
         self.use_friendtree = config.get('use_friendtree',False)
+        self.weight_branch  = config.get('weight_branch', 'xsecWeight' )        
 
     def setDefaultValues(self):
         super().setDefaultValues()
         self.eventweight[0] = 1.0
+        self.goodweight[0] = 1
         
     def prepareStorage(self, output):
         """Set up branch in the output ROOT TTree."""
         output.Branch(f"{self.name}_weight", self.eventweight, f"{self.name}_weight/F")
+        output.Branch(f"{self.name}_weight_isgood", self.goodweight, f"{self.name}_weight_isgood/I")        
     
     def requiredInputs(self):
         """Specify required inputs."""
@@ -36,21 +41,18 @@ class eventWeightProducer(ProducerBaseClass):
         self.setDefaultValues()
         ismc = params.get('ismc',False)
         if not ismc:
-            return {"weight":self.eventweight[0]}
+            return {"weight":self.eventweight[0],"weight_is_good":self.goodweight[0]}
 
         ntuple = data["gen2ntuple"]
         
-        # Reset output variable
-        if not self.use_friendtree:
-            self.eventweight[0] = ntuple.xsecWeight
-        else:
-            self.eventweight[0] = ntuple.weighttree_xsecweight
+        self.eventweight[0] = eval( f'ntuple.{self.weight_branch}' )
 
         #Remove events with infinite event weights
-        if self.eventweight[0] > 100000000000000:
+        if self.eventweight[0] > 100000000000000 or self.eventweight[0]<0.0:
             self.eventweight[0] = 1.0
+            self.goodweight[0]  = 0
         
-        return {"weight":self.eventweight[0]}
+        return {"weight":self.eventweight[0],"weight_is_good":self.goodweight[0]}
 
     def finalize(self):
         """ Nothing to do after event loop. """
